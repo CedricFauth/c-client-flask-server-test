f70b3ca45dbe065754d18730d785cc0361e89d96
Cedric <cedricfauth@yahoo.de>
2020-08-26 17:46:20 +0200

    Update client.c

--- src/client/client.c
+++ src/client/client.c
@@ -18,23 +18,22 @@ int main(int argc, char *argv[]){
 
     printf("\n\rrequest: \t%s\n\rresponse: \t", url);
     
+    curl_global_init(CURL_GLOBAL_ALL);
+    
     CURL *curl = curl_easy_init();
     if(curl) {
-        char errbuf[CURL_ERROR_SIZE];
         
         CURLcode res;
         curl_easy_setopt(curl, CURLOPT_URL, url);
         curl_easy_setopt(curl, CURLOPT_CAINFO, "/home/progfix/myApiCA.pem");
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, stdout);
-        curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, errbuf);
-        errbuf[0] = 0;
 
         res = curl_easy_perform(curl);
-        if(res != 0) fprintf(stderr, "\n%s\n", curl_easy_strerror(res));
-
+        if(res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        
         curl_easy_cleanup(curl);
     }
-
+    curl_global_cleanup();
     free(url);
 
     return 0;
